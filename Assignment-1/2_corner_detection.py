16f4c2a23aad07ca259a7012509e95a285aa05e4
vagdevik <30801221+vagdevik@users.noreply.github.com>
2018-02-08 15:34:30 +0530

    Corner Detection - Final
    Harris - Shi-Tomasi - lambda cornerness

--- Assignment-1/2_corner_detection.py
+++ Assignment-1/2_corner_detection.py
@@ -1,74 +1,114 @@
-import numpy as np
 from matplotlib import pyplot as plt
-import cv2
-import math
 import numpy.linalg as linalg
+from scipy import ndimage,misc
+import numpy as np
+import cv2
 
-global corner_points,threshold
-corner_points=[]
-threshold=900000
+global window_size, alpha, threshold 
+alpha = 0.04
+window_size = 3
+threshold = 0.5
 
-def sobel(gray_image):
-	kernel = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
-	sobelx = cv2.filter2D(gray_image,-1,kernel)
-	print sobelx
-	sobely = cv2.filter2D(gray_image,-1,np.transpose(kernel))
-	return sobelx,sobely
+def readImage(imagePath):
+    image = cv2.imread(imagePath)
+    return image
 
-def hessian_matrix_elements(sobelx,sobely):
-	#Ix = sobelx.astype(np.uint32)
-	#Iy = sobely.astype(np.uint32)
-	Ix = sobelx
-	Iy = sobely
-	print "****"
-	print Iy
-	A = Ix*Ix
-	#B = abs(Ix*Iy)
-	C = np.multiply(Iy,Iy)
-	print "\n"
-	return A,C
-	
-def get_points(A,C,gray_image):	
-	#rows,cols = A.shape
-	det = A*C
-	print det
-	print "--------"
-	trace = A+C
-	print trace
-	print "---------"
-	alpha = 0.03
-	harris_function = (det - (alpha*trace))
-	print harris_function
-	#if harris_function>threshold:
-		
-	rows,cols = harris_function.shape
-	for x in range(rows):
-		for y in range(cols):
-			if harris_function[x][y]>threshold:
-				cv2.circle(gray_image,(x,y), 2, (255,255,255), 1)
-	cv2.imshow("gray_image",gray_image)
-				
-	'''for x in range(rows):
-		for y in range(cols):
-			H = np.array([[A[x][y],B[x][y]],[B[x][y],C[x][y]]])
-			print H
-			print "-----------"
-			eigen_values,_ = linalg.eig(H)
-			print eigen_values
-			if (min(eigen_values!=0)):
-				cv2.circle(gray_image,(x,y), 2, (255,255,255), 1)
-			
-			lambda_min = min(eigen_values)
-			print lambda_min
-	cv2.imshow("gray_image",gray_image)'''		
-			
-gray_image = cv2.imread('Image2.jpg', 0)
-print gray_image
-sobelx,sobely = sobel(gray_image)
+def displayImage(title, image):
+	plt.imshow(image)
+	plt.title(title)
+	plt.show()
+	misc.imsave(str(title)+"___"+str(alpha)+".png",image)
 
-A,C = hessian_matrix_elements(sobelx,sobely)
-get_points(A,C,gray_image)
-print corner_points
+def findGradients(image):
+	I_x = cv2.Sobel(image,cv2.CV_64F,1,0,ksize=5)
+	I_y = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=5)
+	return I_x, I_y
 
-cv2.waitKey(0)                 # Waits forever for user to press any key
-cv2.destroyAllWindows()
+def window_hessian(row, col, center, Ix_square, Iy_square, Ix_Iy):
+        Ix2 = Ix_square[row-center:row+center+1, col-center:col+center+1]
+        Ixy = Ix_Iy[row-center:row+center+1, col-center:col+center+1]
+        Iy2 = Iy_square[row-center:row+center+1, col-center:col+center+1]
+        return Ix2.sum(), Ixy.sum(), Iy2.sum()
+
+def gradient_squares(Ix, Iy):
+	Ix_square = Ix*Ix
+	Iy_square = Iy*Iy
+	Ix_Iy = Ix*Iy
+	return Ix_square, Iy_square, Ix_Iy
+
+def harris_corners(image):
+	global threshold
+	center = window_size/2
+	numRows = image.shape[0]
+	numCols = image.shape[1]
+	harris_img = image.copy()
+	Ix, Iy = findGradients(image)
+	Ix_square, Iy_square, Ix_Iy = gradient_squares(Ix, Iy)
+	R= np.minimum(Ix_square, Iy_square)
+	threshold = threshold * R.max()
+	for row in range(center, numRows-center):
+		for col in range(center, numCols-center):
+			A, B, C = window_hessian(row, col, center, Ix_square, Iy_square, Ix_Iy)
+			Det = A*C -B*B
+			Trace = A + C
+			f = Det - alpha*(Trace*Trace)
+			if f > threshold*100000000:
+				harris_img.itemset((row, col, 0), 0)
+				harris_img.itemset((row, col, 1), 0)
+				harris_img.itemset((row, col, 2), 255)
+	return harris_img
+
+def shi_tomasi_corners(image):
+	global threshold
+	center = window_size/2
+	numRows = image.shape[0]
+	numCols = image.shape[1]
+	st_img = image.copy()
+	Ix, Iy = findGradients(image)
+	Ix_square, Iy_square, Ix_Iy = gradient_squares(Ix, Iy)
+	R= np.minimum(Ix_square, Iy_square)
+	threshold = threshold * R.max()
+	for row in range(center, numRows-center):
+		for col in range(center, numCols-center):
+			A, B, C = window_hessian(row, col, center, Ix_square, Iy_square, Ix_Iy)
+			H = np.array([[A,B],[B,C]])
+			eigen = np.linalg.eigvals(H)
+			f = min(eigen)
+			if f > threshold:
+				st_img.itemset((row, col, 0), 0)
+				st_img.itemset((row, col, 1), 0)
+				st_img.itemset((row, col, 2), 255)
+	return st_img
+
+def lambda_method_corners(image):
+	global threshold
+	center = window_size/2
+	numRows = image.shape[0]
+	numCols = image.shape[1]
+	lambda_img = image.copy()
+	Ix, Iy = findGradients(image)
+	Ix_square, Iy_square, Ix_Iy = gradient_squares(Ix, Iy)
+	R= np.minimum(Ix_square, Iy_square)
+	threshold = threshold * R.max()
+	for row in range(center, numRows-center):
+		for col in range(center, numCols-center):
+			A, B, C = window_hessian(row, col, center, Ix_square, Iy_square, Ix_Iy)
+			H = np.array([[A,B],[B,C]])
+			eigen = np.linalg.eigvals(H)
+			min_eigen = min(eigen)
+			Det = eigen[0]*eigen[1]
+			Trace = eigen[0]+eigen[1]
+			f = Det-(alpha*Trace)
+			if f > threshold*100000000:
+				lambda_img.itemset((row, col, 0), 0)
+				lambda_img.itemset((row, col, 1), 0)
+				lambda_img.itemset((row, col, 2), 255)
+	return lambda_img
+
+image = readImage('butterfly.jpg')
+img = harris_corners(image)
+displayImage("Harris", img)
+img = shi_tomasi_corners(image)
+displayImage("Shi-Tomasi", img)
+img = lambda_method_corners(image)
+displayImage("lambda", img)
