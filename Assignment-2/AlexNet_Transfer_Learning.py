1797b05126094667cc85df84c18505082c970bd4
vagdevik <30801221+vagdevik@users.noreply.github.com>
2018-03-13 00:39:37 +0530

    Modules imported for Transfer Learning

---
+++ Assignment-2/AlexNet_Transfer_Learning.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
